9b3b1b48bcaca31a2848c67fdd02b0a8d3c42c84
taeypark <101627885+gitubanana@users.noreply.github.com>
2024-07-07 19:58:30 +0900

    Title: 두 수의 합, Time: 79ms, Memory: 4MB, Status: Passed - CodeTree

--- 240707/두 수의 합/sum-of-two-num.cpp
+++ 240707/두 수의 합/sum-of-two-num.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-#include <unordered_map>
+#include <map>
+#define NUM first
+#define CNT second
 
-using t_umap = std::unordered_map<int, int>;
+using t_map = std::map<int, int>;
 
 int main()
 {
     std::cin.tie(0)->sync_with_stdio(0);
 
-    int ans = 0;
     int size, goal;
-    t_umap umap;
+    t_map map;
 
     std::cin >> size >> goal;
     while (size--)
@@ -17,8 +18,33 @@ int main()
         int elem;
 
         std::cin >> elem;
-        ans += umap[goal - elem];
-        ++umap[elem];
+        ++map[elem];
+    }
+
+
+    const int &halfGoalCnt = map[goal / 2];
+    int ans = !(goal & 1) * (halfGoalCnt * (halfGoalCnt - 1) / 2);
+    t_map::iterator left = map.begin();
+    t_map::iterator right = map.end();
+    --right;
+
+    while (left != right)
+    {
+        int sum = left->NUM + right->NUM;
+
+        if (sum == goal)
+        {
+            ans += left->CNT * right->CNT;
+            ++left;
+        }
+        else if (sum > goal)
+        {
+            --right;
+        }
+        else
+        {
+            ++left;
+        }
     }
 
     std::cout << ans << '\n';
