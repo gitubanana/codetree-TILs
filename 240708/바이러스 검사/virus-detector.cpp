570f1fa8a37a671f6d1bcfbbc93c482ddb84a557
taeypark <101627885+gitubanana@users.noreply.github.com>
2024-07-08 20:55:44 +0900

    Title: 바이러스 검사, Time: 115ms, Memory: 4MB, Status: Passed - CodeTree

--- 240708/바이러스 검사/virus-detector.cpp
+++ 240708/바이러스 검사/virus-detector.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 #include <vector>
 
+const int MAX_SIZE = 1'000'000;
+
+int toCheck[MAX_SIZE];
+
 int main()
 {
     std::cin.tie(0)->sync_with_stdio(0);
 
     int size;
-    std::vector<int> toCheck;
 
     std::cin >> size;
-    while (size--)
+    for (int i = 0; i < size; ++i)
     {
-        int checkPeople;
-
-        std::cin >> checkPeople;
-        toCheck.push_back(checkPeople);
+        std::cin >> toCheck[i];
     }
 
     int boss, employee;
-    int64_t ans = 0;
+    int64_t ans = size;
 
     std::cin >> boss >> employee;
-    for (const int &cur : toCheck)
+    for (int i = 0; i < size; ++i)
     {
+        int &cur = toCheck[i];
         int withoutBoss = cur - boss;
 
-        ++ans;
         if (withoutBoss > 0)
         {
             ans += withoutBoss / employee
