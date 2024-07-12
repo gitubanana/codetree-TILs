9d18784d3260e0ebea0a145fa68847bf56cd4c52
taeypark <101627885+gitubanana@users.noreply.github.com>
2024-07-12 22:29:09 +0900

    Title: 외주 수익 최대화하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240712/외주 수익 최대화하기/max-of-outsourcing-profit.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+
+struct t_work
+{
+    int day;
+    int pay;
+};
+
+const int MAX_SIZE = 15;
+
+int limitDay;
+int dp[MAX_SIZE + 1];
+t_work works[MAX_SIZE];
+
+int main()
+{
+    std::cin.tie(0)->sync_with_stdio(0);
+
+    std::cin >> limitDay;
+    for (int i = 0; i < limitDay; ++i)
+    {
+        std::cin >> works[i].day >> works[i].pay;
+    }
+
+    for (int curDay = 0; curDay < limitDay; ++curDay)
+    {
+        t_work &curWork = works[curDay];
+        int nextDay = curDay + curWork.day;
+
+        if (nextDay > limitDay)
+            continue ;
+
+        dp[nextDay] = std::max(
+            dp[nextDay],
+            dp[curDay] + curWork.pay
+        );
+    }
+
+    std::cout << *std::max_element(dp, dp + limitDay + 1) << '\n';
+    return 0;
+}
