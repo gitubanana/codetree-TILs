9e40932f44b30518a3f500c64dec0073e38101f0
Taeyang Park <101627885+gitubanana@users.noreply.github.com>
2024-08-01 22:50:48 +0900

    Title: 원자 충돌, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240801/원자 충돌/atom-collision.cpp
+++ 240801/원자 충돌/atom-collision.cpp
@@ -116,7 +116,6 @@ void    composeAll(void)
 
         massSum /= 5;
         speedSum /= nextSpace.size();
-        speedSum %= size;
         nextSpace.clear();
 
         if (massSum == 0)
@@ -141,7 +140,7 @@ int main(void)
         int y, x, mass, speed, dir;
     
         std::cin >> y >> x >> mass >> speed >> dir;
-        CURMAP[y - 1][x - 1].push_back({mass, speed % size, dir});
+        CURMAP[y - 1][x - 1].push_back({mass, speed, dir});
     }
 
     while (move--)
